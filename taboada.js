7af29be086843805a1e1053fbce3f801cec367d8
wfsilva23 <wfsilva23@gmail.com>
2026-05-22 18:11:47 -0300

    primeiro commit

---
+++ taboada.js
@@ -0,0 +1,18 @@
+function Taboada(){
+    var num = window.document.getElementById('num')
+    var end = document.getElementById('end')
+    if (num.value.length == 0 || end.value.length == 0){
+        window.alert('[ERRO] Confira os seus dados')
+    } else {
+    var n = Number(num.value)
+    var e = Number(end.value)
+    var x = 0
+    var r = ''
+    var res = window.document.getElementById('resposta')
+    res.innerHTML = `A tabuada do ${n} é: <br> `
+    do{
+        r = n*x
+        res.innerHTML += `${x} X ${n} = ${r} <br>`
+        x++
+    } while (x <= e)
+}}
